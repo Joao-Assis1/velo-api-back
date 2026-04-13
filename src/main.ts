738e962ecc5010907f8fbe2d0f16e00e674a8b74
Joao-Assis1 <joao_assis_santos@proton.me>
2026-04-13 17:18:05 -0400

    chore: atualizando porta padrão para 3000 e aumentando gitignore

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+  app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -25,11 +27,11 @@ async function bootstrap() {
     .addTag('lessons')
     .addTag('payments')
     .build();
-  
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = Number(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT ?? 3001);
 
   await app.listen(port);
   Logger.log(
